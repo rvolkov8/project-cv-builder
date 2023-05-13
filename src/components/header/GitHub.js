bd57d4386c28af6d60be51966153551fb3898b0f
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-05-13 18:48:53 +0300

    Add a link to my GitHub profile

--- src/components/header/GitHub.js
+++ src/components/header/GitHub.js
@@ -7,8 +7,19 @@ class GitHub extends Component {
   render() {
     return (
       <div className="github-container">
-        <div className="nickname">rvolkov</div>
-        <Icon path={mdiGithub} size={1.2} color="#d7dadc" />
+        <a
+          href="https://github.com/rvolkov8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="nickname">rvolkov8</div>
+          <Icon
+            className="github-icon"
+            path={mdiGithub}
+            size={1.2}
+            color="#d7dadc"
+          />
+        </a>
       </div>
     );
   }
